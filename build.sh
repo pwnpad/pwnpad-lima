82217e2acf29aee64e9e5b094152c7c4d0a948ec
Platy <daryl.limkz@gmail.com>
2025-07-03 19:23:58 +0800

    fix: updated paths for build

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-limactl create --yes ./pwnpad.yml
+CONFIG_DIR="./config"
+ANSIBLE_INVENTORY_DIR="./ansible/inventory"
+ANSIBLE_PLAYBOOK_DIR="./ansible/playbook"
+
+limactl create --yes $CONFIG_DIR/pwnpad.yaml
 limactl start pwnpad
 
-ansible-playbook -i inventory.yml resize_disk.yml
+ansible-playbook -i $ANSIBLE_INVENTORY_DIR/inventory.yml $ANSIBLE_PLAYBOOK_DIR/resize_disk.yml
 limactl restart pwnpad
 
-ansible-playbook -i inventory.yml configure_pwnpad.yml
+ansible-playbook -i $ANSIBLE_INVENTORY_DIR/inventory.yml $ANSIBLE_PLAYBOOK_DIR/configure_pwnpad.yml
 limactl restart pwnpad
